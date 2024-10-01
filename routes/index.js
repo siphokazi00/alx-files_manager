922f1f4a786f8f1b01064646857ed4cf29080215
Siphokazi <usphow@gmail.com>
2024-10-01 12:43:40 -0700

    Authenticate user

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const AppController = require('../controllers/AppController');
+const UsersController = require('../controllers/UsersController');
+const AuthController = require('../controllers/AuthController');
 
 // Define routes
 router.get('/status', AppController.getStatus);
@@ -8,5 +10,10 @@ router.get('/stats', AppController.getStats);
 
 // New route to add a user
 router.post('/users', UsersController.postNew);
+
+// Auth and user routes
+router.get('/connect', AuthController.getConnect);
+router.get('/disconnect', AuthController.getDisconnect);
+router.get('/users/me', UsersController.getMe);
 
 module.exports = router;
