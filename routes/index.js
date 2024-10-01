a6de9256e4429acb836938d7f54b69d837e9765e
Siphokazi <usphow@gmail.com>
2024-10-01 12:16:24 -0700

    First API

---
+++ routes/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const AppController = require('../controllers/AppController');
+
+// Define routes
+router.get('/status', AppController.getStatus);
+router.get('/stats', AppController.getStats);
+
+module.exports = router;
