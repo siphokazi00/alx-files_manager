eace40e69e0d44596b5b2e7f74f1d7ed144b9688
Siphokazi <usphow@gmail.com>
2024-10-01 13:15:42 -0700

    final implementation

--- routes/index.js
+++ routes/index.js
@@ -1,26 +1,20 @@
 const express = require('express');
 const router = express.Router();
-const AppController = require('../controllers/AppController');
 const UsersController = require('../controllers/UsersController');
-const AuthController = require('../controllers/AuthController');
 const FilesController = require('../controllers/FilesController');
 
-// Define routes
-router.get('/status', AppController.getStatus);
-router.get('/stats', AppController.getStats);
-
-// New route to add a user
-router.post('/users', UsersController.postNew);
-
-// Auth and user routes
-router.get('/connect', AuthController.getConnect);
-router.get('/disconnect', AuthController.getDisconnect);
+// User routes
+router.post('/users', UsersController.postUser);
+router.get('/connect', UsersController.getConnect);
+router.get('/disconnect', UsersController.getDisconnect);
 router.get('/users/me', UsersController.getMe);
 
-// Endpoint for file upload
-router.post('/files', FilesController.postUpload);
-
-router.get('/files/:id', FilesController.getShow);
-router.get('/files', FilesController.getIndex);
+// File routes
+router.post('/files', FilesController.postFile);
+router.get('/files/:id', FilesController.getFileById);
+router.get('/files', FilesController.getAllFiles);
+router.put('/files/:id/publish', FilesController.putPublish);
+router.put('/files/:id/unpublish', FilesController.putUnpublish);
+router.get('/files/:id/data', FilesController.getFile);
 
 module.exports = router;
